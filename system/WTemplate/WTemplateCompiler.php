837b6e7d7fac15529fb3b63caa747b963d8466b9
Julien Blatecky <julien1619@gmail.com>
2012-12-30 18:58:43 +0100

    Updating WTemplateCompiler and WTemplateParser documentation

--- system/WTemplate/WTemplateCompiler.php
+++ system/WTemplate/WTemplateCompiler.php
@@ -1,13 +1,9 @@
-<?php defined('IN_WITY') or die('Access denied');
+<?php 
 /**
- * WTemplate
- * Moteur de template pour le CMS Wity
- *
- * @author     Fofif
- * @version    $Id: WTemplate/WTemplateCompiler.php 0006 22-11-2012 Fofif $
- * @package    Wity
- * @subpackage WTemplate
+ * WTemplateCompiler.php
  */
+
+defined('IN_WITY') or die('Access denied');
 
 /**
  * WTemplateCompiler
@@ -17,25 +13,38 @@
  * Each <code>{exemple}</code> is called a "node".
  * In this case, "exemple" is the name of the node.
  * For a closing node, such as <code>{/exemple}</code>, the node name is "exemple_close".
+ *
+ * @package WTemplate
+ * @author Johan Dufau <johandufau@gmail.com>
+ * @version 0.3-22-11-2012
  */
-
 class WTemplateCompiler {
-	/**
-	 * List of tags opened so that we can check there is no jam in opening and closing the nodes
-	 */
+	
+    /**
+     *
+     * @var array List of tags opened so that we can check there is no jam in opening and closing the nodes
+     */
 	private $openNodes = array();
 	
+    /**
+     *
+     * @var array List of all datas used in the compiled file
+     */
 	private $data = array();
 	
+    /**
+     *
+     * @var array List of all registered external compilers  
+     */
 	private static $external_compilers = array();
 	
-	/**
-	 * Define an external compiler
-	 * 
-	 * @param string   $compiler_name Node name (without brackets)
-	 * @param callback $callback      Compiler to call
-	 * @static
-	 */
+    /**
+     * Registers an external compiler
+     * 
+     * @param string $node_name a node name (without brackets)
+     * @param string $callback  the compiler to call
+     * @throws Exception
+     */
 	public static function registerCompiler($node_name, $callback) {
 		if (is_callable($callback)) {
 			if (!isset(self::$external_compilers[$node_name])) {
@@ -55,6 +64,14 @@ class WTemplateCompiler {
 	 * 
 	 * @return string File compiled
 	 */
+    /**
+     * Compiles an entire file using the parser
+     * 
+     * @param string    $string the string that will be compiled
+     * @param array     $data   datas that will be used in the compiled file
+     * @return string the compiled file
+     * @throws Exception
+     */
 	public function compileString($string, array $data = array()) {
 		// clear open tags
 		$this->openNodes = array();
@@ -74,9 +91,13 @@ class WTemplateCompiler {
 		return $code;
 	}
 	
-	/**
-	 * Compilation d'un élément bien précis
-	 */
+    /**
+     * Compiles a node
+     * 
+     * @param string $node a node that will be compiled
+     * @return string the compiled node
+     * @throws Exception
+     */
 	public function compileNode($node) {
 		$node = trim($node);
 		if (empty($node)) {
@@ -144,10 +165,14 @@ class WTemplateCompiler {
 		return $output;
 	}
 	
-	/**
-	 * Compile vars having this format {$var.index1[.index2...]|func1[|func2...]} into php format
-	 * Several levels of vars are managed (for instance: {$var1.{$var2.x}}
-	 */
+    /**
+     * Compile vars having this format {$var.index1[.index2...]|func1[|func2...]} into php format
+     * 
+     * Several levels of vars are managed (for instance: {$var1.{$var2.x}}
+     * 
+     * @param string $string a string that will be compiled
+     * @return string the compiled string
+     */
 	public static function parseVar($string) {
 		if (strpos($string, '$') !== 0) {
 			return;
@@ -192,13 +217,25 @@ class WTemplateCompiler {
 		return $return;
 	}
 	
+    /**
+     * Replaces all variable names by their values
+     * 
+     * @see WTemplateParser::replaceNodes()
+     * @param string $string a string in which variable names will be replaced by their values
+     * @return string a string with values instead of variable names
+     */
 	public static function replaceVars($string) {
 		return WTemplateParser::replaceNodes($string, array('WTemplateCompiler', 'parseVar'));
 	}
 	
-	/**
-	 * Compile vars {$var.index1[.index2...]|func1[|func2...]}
-	 */
+    /**
+     * Compile variables
+     * 
+     * <code>{$var.index1[.index2...]|func1[|func2...]}</code>
+     * 
+     * @param string $args a string of variables that will be compiled
+     * @return string the compiled variables
+     */
 	public function compile_var($args) {
 		if (!empty($args)) {
 			$var = $this->parseVar($args);
@@ -208,11 +245,13 @@ class WTemplateCompiler {
 		}
 	}
 	
-	/**
-	 * Compile {include} node to include sub template files
-	 * 
-	 * @param string $file Le fichier à inclure
-	 */
+    /**
+     * Compiles {include} node to include sub template files
+     * 
+     * @todo Add a recursive compiler to be able to include multiple application in one page
+     * @param string $file the file to include
+     * @return string the php code displying the compiled file (only variables are compiled)
+     */
 	public function compile_include($file) {
 		if (empty($file)) {
 			return '';
@@ -231,6 +270,12 @@ class WTemplateCompiler {
 		return '<?php $this->display("'.$file.'"); ?>';
 	}
 	
+    /**
+     * Compiles {if ...}
+     * 
+     * @param string $args if arguments
+     * @return string the php-if code
+     */
 	public function compile_if($args) {
 		$cond = trim($args);
 		
@@ -240,26 +285,47 @@ class WTemplateCompiler {
 		return '<?php if ('.$cond.'): ?>';
 	}
 	
-	public function compile_else($args) {
+    /**
+     * Compiles {else}
+     * 
+     * @param string $args unused
+     * @return string the php-else code
+     */
+	public function compile_else() {
 		return '<?php else: ?>';
 	}
 	
+    /**
+     * Compiles {elseif ...}
+     * 
+     * @param string $args elseif arguments
+     * @return type the php-elseif code
+     */
 	public function compile_elseif($args) {
 		return str_replace('if', 'elseif', $this->compile_if($args));
 	}
 	
+    /**
+     * Compiles {/if}
+     * 
+     * @return string the php-endif code
+     */
 	public function compile_if_close() {
 		return '<?php endif; ?>';
 	}
 	
-	/**
-	 * Counts the number of {for} in order to make {empty} work properly
-	 */
+    /**
+     *
+     * @var int Counts the number of {for} in order to make {empty} work properly
+     */
 	private $for_count = 0;
 	
-	/**
-	 * {for [$key, ]$value in $array}
-	 */
+    /**
+     * Compiles {for [$key, ]$value in $array}
+     * 
+     * @param string $args for arguments
+     * @return string php-for code
+     */
 	public function compile_for($args) {
 		$matches = array();
 		// RegEx string to search "$key, $value in $array" substring
@@ -288,18 +354,31 @@ class WTemplateCompiler {
 		}
 	}
 	
+    /**
+     * Compiles {/for}
+     * 
+     * @return string php-endforeach code
+     */
 	public function compile_for_close() {
 		$this->for_count--;
 		return '<?php endforeach; ?>';
 	}
 	
-	/**
-	 * Case of emptiness of last for's array being iterated
-	 */
-	public function compile_empty($args) {
+    /**
+     * Compiles empty in case of emptiness of last for's array being iterated
+     * 
+     * @todo Write a better comment because I don't understand when this method is used
+     * @return string php-empty code
+     */
+	public function compile_empty() {
 		return "<?php if (isset(\$hidden_counter".($this->for_count+1).") && intval(\$hidden_counter".($this->for_count+1).") == 0): ?>";
 	}
 	
+    /**
+     * Compiles empty_close
+     * 
+     * @return string php-empty_close code
+     */
 	public function compile_empty_close() {
 		return "<?php endif; ?>";
 	}
@@ -308,6 +387,15 @@ class WTemplateCompiler {
 	 * {set $a = 5}
 	 * {set {$a} = {$b} + 1}
 	 */
+    /**
+     * Compiles an assignement
+     * 
+     * <code>{set $a = 5}
+	 * {set {$a} = {$b} + 1}</code>
+     * 
+     * @param string $args an assignement <code>var=value</code>
+     * @return string the php-assignement code
+     */
 	public function compile_set($args) {
 		$a = explode('=', $args);
 		if (count($a) != 2) {
