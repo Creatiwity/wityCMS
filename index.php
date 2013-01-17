c689498efada700543d929b4da8ed1707a9d54ac
Julien Blatecky <julien1619@gmail.com>
2013-01-17 14:01:06 +0100

    Adds installer instanciation if exists

--- index.php
+++ index.php
@@ -17,6 +17,15 @@ define('IN_WITY', true);
 define('WITY_VERSION', '0.3.0');
 
 /**
+ * Installer section
+ */
+if(file_exists("installer")) {
+    require 'installer/installer.php';
+    $installer = new Installer();
+    return;
+}
+
+/**
  * Error reporting level = MAXIMUM
 */
 error_reporting(E_ALL);
