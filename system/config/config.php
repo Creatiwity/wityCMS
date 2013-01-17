135cea63eea897a21926499da8154872055a23ab
Julien Blatecky <julien1619@gmail.com>
2013-01-17 14:00:44 +0100

    Fix format

--- system/config/config.php
+++ system/config/config.php
@@ -1,15 +1,14 @@
 <?php
+
 /**
  * Main configuration file
  * 
  * @package System
  */
-
 $config = array(
     'base' => 'http://localhost/Wity-Lite/',
-	'site_name' => 'Wity Lite',
-	'theme' => '_blank',
-	'lang' => 'fr-FR',
+    'site_name' => 'Wity Lite',
+    'theme' => '_blank',
+    'lang' => 'fr-FR',
 );
-
 ?>
