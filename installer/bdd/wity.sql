c0ed1fcb4f399adb63e81101bf02bd4be90fe024
Julien Blatecky <julien1619@gmail.com>
2013-02-05 20:55:31 +0100

    New db structure

--- installer/bdd/wity.sql
+++ installer/bdd/wity.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.5
+-- version 3.4.10.1
 -- http://www.phpmyadmin.net
 --
--- Client: localhost
--- Généré le : Ven 30 Décembre 2011 à 14:48
--- Version du serveur: 5.5.16
--- Version de PHP: 5.3.8
+-- Host: localhost
+-- Generation Time: Jan 31, 2013 at 12:36 PM
+-- Server version: 5.5.20
+-- PHP Version: 5.3.10
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,31 +17,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de données: `wity`
+-- Database: `test`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `contact_mails`
---
-
-CREATE TABLE IF NOT EXISTS `contact_mails` (
-  `id` int(9) unsigned NOT NULL AUTO_INCREMENT,
-  `userid` int(9) DEFAULT NULL,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `name` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `organisme` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
-  `email` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `objet` tinytext CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `message` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=75 ;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `news`
+-- Table structure for table `news`
 --
 
 CREATE TABLE IF NOT EXISTS `news` (
@@ -51,65 +33,65 @@ CREATE TABLE IF NOT EXISTS `news` (
   `author` varchar(30) CHARACTER SET latin1 NOT NULL,
   `content` text CHARACTER SET latin1 NOT NULL,
   `keywords` mediumtext CHARACTER SET latin1 NOT NULL,
-  `cat` tinytext NOT NULL,
+  `cat` tinytext CHARACTER SET utf8 NOT NULL,
   `date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `editor_id` int(11) NOT NULL,
   `views` int(11) NOT NULL DEFAULT '0',
   `publier` tinyint(1) NOT NULL DEFAULT '0',
-  `image` tinytext NOT NULL,
+  `image` tinytext CHARACTER SET utf8 NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=14 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `newsletters`
+-- Table structure for table `newsletters`
 --
 
 CREATE TABLE IF NOT EXISTS `newsletters` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `userid` int(11) NOT NULL,
-  `de` varchar(100) NOT NULL,
-  `objet` varchar(255) NOT NULL,
-  `message` text NOT NULL,
-  `destinataires` text NOT NULL,
+  `de` varchar(100) CHARACTER SET latin1 NOT NULL,
+  `objet` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `message` text CHARACTER SET latin1 NOT NULL,
+  `destinataires` text CHARACTER SET latin1 NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `cc` tinytext NOT NULL,
-  `cci` tinytext NOT NULL,
-  `attachment` varchar(255) NOT NULL,
+  `cc` tinytext CHARACTER SET latin1 NOT NULL,
+  `cci` tinytext CHARACTER SET latin1 NOT NULL,
+  `attachment` varchar(255) CHARACTER SET latin1 NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=27 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `news_cats`
+-- Table structure for table `news_cats`
 --
 
 CREATE TABLE IF NOT EXISTS `news_cats` (
   `cid` tinyint(11) NOT NULL AUTO_INCREMENT,
   `name` tinytext CHARACTER SET latin1 NOT NULL,
-  `shortname` tinytext NOT NULL,
+  `shortname` tinytext CHARACTER SET utf8 NOT NULL,
   `parent` tinyint(4) NOT NULL,
   PRIMARY KEY (`cid`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=22 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `news_cats_relations`
+-- Table structure for table `news_cats_relations`
 --
 
 CREATE TABLE IF NOT EXISTS `news_cats_relations` (
   `news_id` mediumint(9) NOT NULL,
   `cat_id` tinyint(4) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `pages`
+-- Table structure for table `pages`
 --
 
 CREATE TABLE IF NOT EXISTS `pages` (
@@ -122,58 +104,44 @@ CREATE TABLE IF NOT EXISTS `pages` (
   `creation_time` int(11) NOT NULL,
   `edit_time` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=19 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `shoutbox`
---
-
-CREATE TABLE IF NOT EXISTS `shoutbox` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `userid` int(11) NOT NULL,
-  `nickname` varchar(50) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `message` text NOT NULL,
-  UNIQUE KEY `id` (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=32 ;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nickname` varchar(100) CHARACTER SET latin1 NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `password` varchar(50) CHARACTER SET utf8 NOT NULL,
   `confirm` int(11) NOT NULL DEFAULT '0',
   `email` varchar(100) CHARACTER SET latin1 NOT NULL,
-  `firstname` varchar(100) NOT NULL,
-  `lastname` varchar(100) NOT NULL,
+  `firstname` varchar(100) CHARACTER SET utf8 NOT NULL,
+  `lastname` varchar(100) CHARACTER SET utf8 NOT NULL,
+  `country` varchar(25) CHARACTER SET utf8 NOT NULL,
   `groupe` int(4) NOT NULL,
   `access` text CHARACTER SET latin1 NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `last_activity` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `ip` varchar(50) NOT NULL,
+  `ip` varchar(50) CHARACTER SET utf8 NOT NULL,
   KEY `id` (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=241 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `users_cats`
+-- Table structure for table `users_groups`
 --
 
-CREATE TABLE IF NOT EXISTS `users_cats` (
+CREATE TABLE IF NOT EXISTS `users_groups` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `parent` mediumint(9) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `access` text NOT NULL,
+  `name` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `access` text CHARACTER SET latin1 NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
