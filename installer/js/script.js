c4c7110de66776bfe23ee815edc9dcfa706a4df7
Julien Blatecky <julien1619@gmail.com>
2013-03-20 00:03:22 +0100

    Preparing the javascript class to manage the Installer

--- installer/js/script.js
+++ installer/js/script.js
@@ -1,6 +1,40 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+$(document).ready(function() {
+    var Installer, Step, Group, Field;
 
+    Installer = (function() {
+        function Installer() {
+            //Create the global manager
+        }
 
+        /*Installer.prototype.ma = function() {
+
+        };*/
+
+        return Installer;
+    })();
+    
+    Step = (function() {
+        function Step() {
+            //Build the group list
+        }
+        
+        return Step;
+    })();
+    
+    Group = (function() {
+        function Group() {
+            //Build the field list
+            //Stores url (command)
+        }
+        
+        return Group;
+    })();
+    
+    Field = (function() {
+        function Field() {
+            //
+        }
+        
+        return Field;
+    })();
+});
