ac7a00b1c47dcd08bb4c5e9bc2f5efebe82fdd7a
Julien Blatecky <julien1619@gmail.com>
2013-04-28 14:37:45 +0200

    Defining groups validation

--- installer/installer.php
+++ installer/installer.php
@@ -52,8 +52,22 @@ class Installer {
                 self::$view->render();
                 return;
 
+            case 'INIT_INSTALLER':
+                self::$view->info('installer', $data['installer'], 'Installer initialized', 'The Installer has been successfully initialized.');
+                break;
+
+            case 'FINISH_INSTALLATION':
+                if(self::installerValidation($data)) {
+                    //save and success
+                } else {
+                    
+                }
+                break;
+
             // Groups
-            
+            case 'GROUP_VALIDATION':
+                self::groupValidation($data);
+                break;
 
             //Autocompletes
             case 'GET_THEMES':
@@ -82,6 +96,138 @@ class Installer {
         }
 
         self::$view->respond();
+    }
+
+    private static function installerValidation($data) {
+        return false;
+    }
+
+    private static function groupValidation($data) {
+        $respond = true;
+
+        switch ($data['group']) {
+            case 'site_name':
+                $r = Request::getAssoc(array('site_name'), array('site_name'=>''), 'POST');
+                if(self::isVerifiedString($r['site_name'], $data, $respond)) {
+                    self::$view->success('group', $data['group'], "Validated !", "Site name validated.");
+                } else if($respond) {
+                    self::$view->error('group', $data['group'], "Invalid site name", "The site name must be an alphanumeric string. (- and ' and spaces are allowed too)");
+                }
+                break;
+            
+            case 'base_url':
+                $r = Request::getAssoc(array('base'), array('base'=>''), 'POST');
+                if(self::isURL($r['base'], $data, $respond)) {
+                    self::$view->success('group', $data['group'], "Validated !", "Base URL validated.");
+                } else if($respond) {
+                    self::$view->error('group', $data['group'], "Invalid base url", "The base url must be a valid URL representing the constant part of your site URL.");
+                }
+                break;
+
+            case 'theme':
+                $r = Request::getAssoc(array('theme'), array('theme'=>''), 'POST');
+                if(self::isTheme($r['theme'], $data, $respond)) {
+                    self::$view->success('group', $data['group'], "Validated !", "Theme validated.");
+                } else if($respond) {
+                    self::$view->error('group', $data['group'], "Invalid theme", "Theme parameter must be an existing front theme, in 'themes' directory.");
+                }
+                break;
+
+            case 'language':
+                // TODO : auto-detect available languages and validate them
+                self::$view->success('group', $data['group'], "Validated !", "Theme validated.");
+                break;
+
+            case 'front_app':
+                $r = Request::getAssoc(array('default'), array('default'=>''), 'POST');
+                if(self::isFrontApp($r['default'], $data, $respond)) {
+                    self::$view->success('group', $data['group'], "Validated !", "Front application validated.");
+                } else if($respond) {
+                    self::$view->error('group', $data['group'], "Invalid front application", "Starting front application parameter must an existing front application, in 'apps' directory.");
+                }
+                break;
+
+            case 'admin_app':
+                $r = Request::getAssoc(array('admin'), array('admin'=>''), 'POST');
+                if(self::isAdminApp($r['admin'], $data, $respond)) {
+                    self::$view->success('group', $data['group'], "Validated !", "Admin application validated.");
+                } else if($respond) {
+                    self::$view->error('group', $data['group'], "Invalid admin application", "Starting admin application parameter must an existing admin application, in 'apps' directory.");
+                }
+                break;
+
+            case 'db_credentials':
+                $r = Request::getAssoc(array('server', 'port', 'user', 'pw'), array('server'=>'', 'port'=>'', 'user'=>'', '', 'pw'=>''), 'POST');
+                if(self::isSQLServer($r, $data, $respond)) {
+                    self::$view->success('group', $data['group'], "Validated !", "Database credentials validated.");
+                } else if($respond) {
+                    self::$view->error('group', $data['group'], "Invalid database credentials", "Unable to connect to the database with the credentials you've just provided.");
+                }
+                break;
+
+            case 'db_name':
+                $r = Request::getAssoc(array('server', 'port', 'user', 'pw', 'dbname'), array('server'=>'', 'port'=>'', 'user'=>'', '', 'pw'=>'', 'dbname'=>''), 'POST');
+                if(self::isDatabase($r, $data, $respond)) {
+                    self::$view->success('group', $data['group'], "Validated !", "Database name validated.");
+                } else if($respond) {
+                    self::$view->error('group', $data['group'], "Invalid database name", "Unable to find the database with the name you've just provided.");
+                }
+                break;
+
+            case 'tables_prefix':
+                $r = Request::getAssoc(array('server', 'port', 'user', 'pw', 'dbname', 'prefix'), array('server'=>'', 'port'=>'', 'user'=>'', '', 'pw'=>'', 'dbname'=>'', 'prefix'=>''), 'POST');
+                if(self::isPrefixNotExisting($r, $data, $respond)) {
+                    self::$view->success('group', $data['group'], "Validated !", "Tables prefix validated and not used.");
+                } else if($respond) {
+                    self::$view->warning('group', $data['group'], "Prefix already used", "Be careful, the prefix you provides is already used. Some existing tables will be overridden");
+                }
+                break;
+
+            case 'user_nickname':
+                $r = Request::getAssoc(array('nickname'), array('nickname'=>''), 'POST');
+                if(self::isVerifiedString($r['nickname'], $data, $respond)) {
+                    self::$view->success('group', $data['group'], "Validated !", "Nickname validated.");
+                } else if($respond) {
+                    self::$view->error('group', $data['group'], "Invalid nickname", "Your nickname must be an alphanumeric string. (- and ' and spaces are allowed too)");
+                }
+                break;
+
+            case 'user_password':
+                $r = Request::getAssoc(array('password'), array('password'=>''), 'POST');
+                self::$view->success('group', $data['group'], "Validated !", "Password validated.");
+                break;
+
+            case 'user_email':
+                $r = Request::getAssoc(array('email'), array('email'=>''), 'POST');
+                if(self::isEmail($r['email'], $data, $respond)) {
+                    self::$view->success('group', $data['group'], "Validated !", "Email validated.");
+                } else if($respond) {
+                    self::$view->error('group', $data['group'], "Invalid email", "This email is not valid.");
+                }
+                break;
+
+            case 'user_firstname':
+                $r = Request::getAssoc(array('firstname'), array('firstname'=>''), 'POST');
+                if(self::isVerifiedString($r['firstname'], $data, $respond)) {
+                    self::$view->success('group', $data['group'], "Validated !", "Firstname validated.");
+                } else if($respond) {
+                    self::$view->error('group', $data['group'], "Invalid firstname", "Your firstname must be an alphanumeric string. (- and ' and spaces are allowed too)");
+                }
+                break;
+
+            case 'user_lastname':
+                $r = Request::getAssoc(array('lastname'), array('lastname'=>''), 'POST');
+                if(self::isVerifiedString($r['lastname'], $data, $respond)) {
+                    self::$view->success('group', $data['group'], "Validated !", "Lastname validated.");
+                } else if($respond) {
+                    self::$view->error('group', $data['group'], "Invalid lastname", "Your lastname must be an alphanumeric string. (- and ' and spaces are allowed too)");
+                }
+                break;
+
+            default:
+                self::$view->error('step', $data['step'], 'Unknown group', "You're trying to validate an unknown group.");
+                return false;
+        }
     }
 
     /**
@@ -114,7 +260,7 @@ class Installer {
         }
 
         $dsn = 'mysql:dbname=;host='.WConfig::get('database.server');
-        $dsn .= (isset($credentials['port']) && !empty($credentials['port']) && is_numeric($credentials['port'])) ? ';port='.$credentials['port']):'';
+        $dsn .= (isset($credentials['port']) && !empty($credentials['port']) && is_numeric($credentials['port'])) ? ';port='.$credentials['port']:'';
 
         try {
             new PDO($dsn, $credentials['user'], $$credentials['pw']);
@@ -142,7 +288,7 @@ class Installer {
         }
 
         $dsn = 'mysql:dbname=;host='.WConfig::get('database.server');
-        $dsn .= (isset($credentials['port']) && !empty($credentials['port']) && is_numeric($credentials['port'])) ? ';port='.$credentials['port']):'';
+        $dsn .= (isset($credentials['port']) && !empty($credentials['port']) && is_numeric($credentials['port'])) ? ';port='.$credentials['port']:'';
 
         try {
             new PDO($dsn, $credentials['user'], $$credentials['pw']);
@@ -173,7 +319,7 @@ class Installer {
         }
 
         $dsn = 'mysql:dbname=;host='.WConfig::get('database.server');
-        $dsn .= (isset($credentials['port']) && !empty($credentials['port']) && is_numeric($credentials['port'])) ? ';port='.$credentials['port']):'';
+        $dsn .= (isset($credentials['port']) && !empty($credentials['port']) && is_numeric($credentials['port'])) ? ';port='.$credentials['port']:'';
 
         try {
             $db = new PDO($dsn, $credentials['user'], $$credentials['pw']);
@@ -194,7 +340,7 @@ class Installer {
         $prep = $db->prepare("SHOW TABLES LIKE :prefixedTable");
         $prep->bindParam(":prefixedTable", $prefix."user");
         $prep->execute();
-        return $prep->fetch() ? false;true;
+        return $prep->fetch() ? false:true;
     }
 
     private static function isEmail($email, $data, &$respond) {
