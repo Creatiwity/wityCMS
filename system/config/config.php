983f365ebc3a2f28624646f34c3dc7b9a07362bb
Julien Blatecky <julien1619@gmail.com>
2013-04-30 19:40:34 +0200

    Updating config.php

--- system/config/config.php
+++ system/config/config.php
@@ -1,14 +1,17 @@
 <?php
-
 /**
  * Main configuration file
  * 
  * @package System
  */
+
 $config = array(
-    'base' => 'http://localhost/Wity-Lite/',
-    'site_name' => 'Wity Lite',
-    'theme' => '_blank',
-    'lang' => 'fr-FR',
+	'base' => 'http://localhost/Wity-Lite/',
+	'site_name' => 'WityCMS',
+	'theme' => '_blank',
+	'lang' => 'fr-FR',
+	'email' => 'contact@site.fr',
+	'debug' => true,
 );
+
 ?>
