62b19088c6eb323c3ec33579bc9e9e08e55be2c9
Julien Blatecky <julien1619@gmail.com>
2013-09-14 15:15:08 +0200

    Updating doc builder

--- makedoc.bat
+++ makedoc.bat
@@ -1,3 +1,3 @@
 @echo off
 set PWD=%~dp0
-%PHP_PEAR_BIN_DIR%\phpdoc -d %PWD% -t %PWD%doc --title WityCMS -i libraries\ -i apps\
+%PHP_PEAR_BIN_DIR%\phpdoc -d %PWD% -t %PWD%doc --title WityCMS --ignore libraries/,helpers/fpdf/,helpers/phpmailer/,helpers/upload/,helpers/SimpleImage/
