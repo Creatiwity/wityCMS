7c904cca41841562ef6846e1f13357fb3b3bf80c
Fofif <johandufau@gmail.com>
2013-09-23 22:40:46 +0200

    Changed include_once to require_once
    System classes are required to launch WityCMS

--- system/WCore/WSystem.php
+++ system/WCore/WSystem.php
@@ -40,7 +40,7 @@ class WSystem {
 	 */
 	public static function getSession() {
 		if (!is_object(self::$sessionInstance)) {
-			include_once SYS_DIR.'WCore/WSession.php';
+			require_once SYS_DIR.'WCore/WSession.php';
 			self::$sessionInstance = new WSession();
 		}
 		
@@ -53,7 +53,7 @@ class WSystem {
 	 */
 	public static function getTemplate() {
 		if (!is_object(self::$templateInstance)) {
-			include_once SYS_DIR.'WTemplate/WTemplate.php';
+			require_once SYS_DIR.'WTemplate/WTemplate.php';
 			try {
 				self::$templateInstance = new WTemplate(WITY_PATH, CACHE_DIR.'templates'.DS);
 			} catch (Exception $e) {
