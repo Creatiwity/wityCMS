1eaa999eac28848a98c4a42dad84544c81ed7079
Julien Blatecky <julien1619@gmail.com>
2013-10-21 14:57:35 +0200

    Updating bootstrap modal and admin imports

---
+++ libraries/bootstrap-modal-2.2.0/js/bootstrap3-patch.js
@@ -0,0 +1,6 @@
+$.fn.modal.defaults.spinner = $.fn.modalmanager.defaults.spinner = 
+    '<div class="loading-spinner" style="width: 200px; margin-left: -100px;">' +
+        '<div class="progress progress-striped active">' +
+            '<div class="progress-bar" style="width: 100%;"></div>' +
+        '</div>' +
+    '</div>';
