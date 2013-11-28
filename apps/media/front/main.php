88dad0e640ae81114c2fd22fd36b640b40c106d0
Julien Blatecky <julien1619@gmail.com>
2013-11-28 11:12:13 +0100

    Extracts folder creation function from upload

--- apps/media/front/main.php
+++ apps/media/front/main.php
@@ -7,7 +7,7 @@ defined('IN_WITY') or die('Access denied');
 
 /**
  * MediaController is the Front Controller of the Media Application
- * 
+ *
  * @package Apps
  * @author Johan Dufau <johan.dufau@creatiwity.net>
  * @author Julien Blatecky <julien.blatecky@creatiwity.net>
@@ -17,7 +17,7 @@ class MediaController extends WController {
 
 	/**
 	 * Returns a list of available resources corresponding to the filter's parameters
-	 * 
+	 *
 	 * @param array $params Input parameters
 	 * @return array An array of available resources
 	 */
@@ -30,7 +30,7 @@ class MediaController extends WController {
 
 	/**
 	 * Stores a file sent with an Ajax request and returns the corresponding id
-	 * 
+	 *
 	 * @param array $params Input parameters
 	 * @return array Unique identifier of the stored resource
 	 */
@@ -42,6 +42,51 @@ class MediaController extends WController {
 		}
 
 		// Test if folders exist and create them
+		if (($c = $this->checkFolders()) != true) {
+			return $c;
+		}
+
+		// Save file
+		/**
+		 * $this->file_max_size
+		 * $this->allowed
+		 * $this->forbidden
+		 * $this->image_max_width
+		 * $this->image_min_width
+		 * $this->image_max_height
+		 * $this->image_min_height
+		 * $this->image_max_ratio
+		 * $this->image_min_ratio
+		 * $this->image_max_pixels
+		 * $this->image_min_pixels
+		 *
+		 * $this->file_new_name_body
+		 * $this->file_new_name_ext
+		 * $this->file_name_body_add
+		 * $this->file_name_body_pre
+		 *
+		 * $this->file_safe_name
+		 * $this->file_dst_pathname
+		 */
+		$h_upload = WHelper::load('upload', array($datas['media_file'], WConfig::get('config.lang')));
+
+		$h_upload->file_safe_name = true;
+		$h_upload->file_dst_pathname = UPLOAD_DIR.'media'.DS.'private';
+
+		$media_params = $this->model->create_new_media($h_upload->file_src_name);
+
+		// Disallow access to anyone but the uploader
+		// Add a UID
+		// Store in DB
+		// Return JSON identifier and link
+	}
+
+	/**
+	 * Returns true if upload folders are correctly created and secured, otherwise returns WNote
+	 *
+	 * @return boolean|WNote
+	 */
+	private function checkFolders() {
 		if (!file_exists(UPLOAD_DIR.'media') || !is_dir(UPLOAD_DIR.'media')) {
 			if (!mkdir(UPLOAD_DIR.'media')) {
 				return WNote::error("unable_to_mkdir_media", WLang::_("server_configuration_error"));
@@ -71,48 +116,16 @@ class MediaController extends WController {
 
 			if (fclose($htaccess_file) === false) {
 				return WNote::error("unable_to_fclose_htaccess", WLang::_("server_configuration_error"));
-			}	
+			}
 		}
 
-		// Save file
-		/**
-		 * $this->file_max_size
-		 * $this->allowed
-		 * $this->forbidden
-		 * $this->image_max_width
-		 * $this->image_min_width
-		 * $this->image_max_height
-		 * $this->image_min_height
-		 * $this->image_max_ratio
-		 * $this->image_min_ratio
-		 * $this->image_max_pixels
-		 * $this->image_min_pixels
-		 * 
-		 * $this->file_new_name_body
-		 * $this->file_new_name_ext
-		 * $this->file_name_body_add
-		 * $this->file_name_body_pre
-		 * 
-		 * $this->file_safe_name
-		 * $this->file_dst_pathname
-		 */
-		$h_upload = WHelper::load('upload', array($datas['media_file'], WConfig::get('config.lang')));
-
-		$h_upload->file_safe_name = true;
-		$h_upload->file_dst_pathname = UPLOAD_DIR.'media'.DS.'private';
-
-		$media_params = $this->model->create_new_media($h_upload->file_src_name);
-
-		// Disallow access to anyone but the uploader
-		// Add a UID
-		// Store in DB
-		// Return JSON identifier and link
+		return true;
 	}
 
 	/**
 	 * Returns the logic needed to call a media upload in a view
-	 * 
-	 * @param array $params Input parameters 
+	 *
+	 * @param array $params Input parameters
 	 * @return array Nothing
 	 */
 	protected function upload_button(array $params) {
@@ -121,7 +134,7 @@ class MediaController extends WController {
 
 	/**
 	 * Edits the meta informations of a given resource
-	 * 
+	 *
 	 * @param array $params New informations and id of the resource to edit
 	 * @return array Nothing
 	 */
@@ -133,7 +146,7 @@ class MediaController extends WController {
 
 	/**
 	 * Returns the sources which relates to a given resource
-	 * 
+	 *
 	 * @param array $params Input parameters
 	 * @return array Array of resources which relates to a given source
 	 */
@@ -144,7 +157,7 @@ class MediaController extends WController {
 
 	/**
 	 * Returns the link to a given resource
-	 * 
+	 *
 	 * @param array $params Input parameters
 	 * @return array The link which will be used to access to a resource
 	 */
@@ -153,7 +166,7 @@ class MediaController extends WController {
 		// Otherwise, find the resource id (in the name file name.this_is_the_id_of_this_file.ext)
 		// Returns (name.ext?=this_is_the_id_of_this_file)
 	}
-	
+
 }
 
 ?>
