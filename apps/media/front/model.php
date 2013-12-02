7997c5ee367db6411243f57fba65d293df247b37
Julien Blatecky <julien1619@gmail.com>
2013-12-02 10:24:13 +0100

    Creating media id and data in database

--- apps/media/front/model.php
+++ apps/media/front/model.php
@@ -40,7 +40,7 @@ class MediaModel {
 
 	public function generateFileID($hash, $length = 8) {
 		while(!isset($id) || $this->fileIDExists($id)) {
-			$id = generateAnID($hash, $length);
+			$id = $this->generateAnID($hash, $length);
 		}
 
 		return $id;
@@ -63,22 +63,74 @@ class MediaModel {
 		$uid = base64_encode($pack);
 
 		// mixed case
-		$uid = ereg_replace("[^A-Za-z0-9]", "", $uid);
+		$uid = preg_replace("#[^A-Za-z0-9]+#", "", $uid);
 
-		if ($len < 4) {
-			$len = 4;
+		if ($length < 4) {
+			$length = 4;
 		}
 
-		if ($len > 128) {
+		if ($length > 128) {
 			// prevent silliness, can remove
-			$len = 128;
+			$length = 128;
 		}
 
-		while (strlen($uid) < $len) {
+		while (strlen($uid) < $length) {
 			// append until length achieved
 			$uid = $uid.$this->generateFileID($hash, 22);
 		}
 
-		return substr($uid, 0, $len);
+		return substr($uid, 0, $length);
+	}
+
+	/**
+	 * Creates the media instance in db
+	 *
+	 * @param array $params
+	 * @return boolean True if insertion worked
+	 */
+	public function createNewMedia($params) {
+		// Insert data in table
+		$prep = $this->db->prepare('
+			INSERT INTO media_list(fileID, hash, filename, mime, extension, state)
+			VALUES(:fileID, :hash, :filename, :mime, :extension, :state)
+		');
+
+		$prep->bindParam(':fileID', $params['fileID']);
+		$prep->bindParam(':hash', $params['hash']);
+		$prep->bindParam(':filename', $params['filename']);
+		$prep->bindParam(':mime', $params['mime']);
+		$prep->bindParam(':extension', $params['extension']);
+		$prep->bindParam(':state', $params['state']);
+
+		return $prep->execute();
+	}
+
+	public function getMediaData($fileID, $onlyOnline = true) {
+		// Insert data in table
+		$prep = $this->db->prepare('
+			SELECT fileID, hash, filename, mime, extension, state
+			INTO media_list
+			WHERE fileID = :fileID'.($onlyOnline ? ' AND state = "ONLINE"':'')
+		);
+
+		$prep->bindParam(':fileID', $fileID);
+		$prep->execute();
+
+		return $prep->fetch(PDO::FETCH_ASSOC);
+	}
+
+	/**
+	 * Returns the complete filename if it exists, access is allowed and compare sha1 if needed
+	 *
+	 * @param array $params
+	 * @return string|boolean Returns false if there is no file corresponding,
+	 * 						'corrupted' if file is corrupted or the complete filename
+	 */
+	public function getFile($params) {
+		// Build complete filename (private and public)
+		// Do file_exists
+		// Test access for this user (only if private)
+		// If needed test sha1 with the db one and/or the one in $params
+		// Returns complete filename
 	}
 }
