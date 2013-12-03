9c5430a5b4d36603f546abae031834643ebb6187
Julien Blatecky <julien1619@gmail.com>
2013-12-03 12:07:35 +0100

    Add 'require' global variable and insert requirejs code in the final render

--- system/WCore/WView.php
+++ system/WCore/WView.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * WView.php
  */
@@ -7,7 +7,7 @@ defined('WITYCMS_VERSION') or die('Access denied');
 
 /**
  * WView handles application's Views.
- * 
+ *
  * @package System\WCore
  * @author Johan Dufau <johan.dufau@creatiwity.net>
  * @version 0.4.0-12-10-2013
@@ -17,55 +17,56 @@ class WView {
 	 * @var array Context of the application describing app's name, app's directory and app's main class
 	 */
 	private $context = array();
-	
+
 	/**
 	 * @var WTemplate Instance of WTemplate
 	 */
 	public $tpl;
-	
+
 	/**
 	 * @var string Theme name for this view
 	 */
 	private $theme;
-	
+
 	/**
 	 * @var string Template file to be used when the view will be rendered
 	 */
 	private $templateFile = '';
-	
+
 	/**
 	 * @var array Global variables with a special treatment like "css" and "js"
 	 */
 	private static $global_vars = array(
-		'css' => array(),
-		'js' => array()
+		'css'     => array(),
+		'js'      => array(),
+		'require' => array()
 	);
-	
+
 	/**
 	 * @var array Template variables
 	 */
 	protected $vars = array();
-	
+
 	/**
 	 * @var Final view rendered as a string
 	 */
 	private $rendered_string;
-	
+
 	/**
 	 * @var Counts the number of times a view was rendered for each signature.
 	 */
 	private $render_counts = array();
-	
+
 	/**
 	 * Setup template
 	 */
 	public function __construct() {
 		$this->tpl = WSystem::getTemplate();
 	}
-	
+
 	/**
 	 * Defines the context of the application this View belongs to
-	 * 
+	 *
 	 * @param array  $context  Context of the application describing app's name, app's directory and app's main class
 	 */
 	public function setContext($context) {
@@ -73,23 +74,23 @@ class WView {
 			$this->context = $context;
 		}
 	}
-	
+
 	/**
 	 * Returns the application context
-	 * 
+	 *
 	 * @return array Application's context
 	 */
 	public function getContext($field = '') {
 		if (!empty($field)) {
 			return (isset($this->context[$field])) ? $this->context[$field] : '';
 		}
-		
+
 		return $this->context;
 	}
-	
+
 	/**
 	 * Updates the signature of the view.
-	 * 
+	 *
 	 * @param string $value Signature of the view to display in forms
 	 */
 	public function setSignature($value) {
@@ -97,10 +98,10 @@ class WView {
 			$this->context['signature'] = $value;
 		}
 	}
-	
+
 	/**
 	 * Gets the name of the current view
-	 * 
+	 *
 	 * @return string View name
 	 */
 	public function getName() {
@@ -110,10 +111,10 @@ class WView {
 			return get_class($this);
 		}
 	}
-	
+
 	/**
 	 * Sets the file that will be used for template compiling
-	 * 
+	 *
 	 * @param string $file file that will be used for template compiling
 	 */
 	public function setTemplate($file) {
@@ -121,12 +122,12 @@ class WView {
 		if (DS != '/') {
 			$file = str_replace('/', DS, $file);
 		}
-		
+
 		// Format the file asked
 		if (strpos($file, DS) === false) {
 			$file = $this->context['directory'].'templates'.DS.$file.'.html';
 		}
-		
+
 		$file = str_replace(WITY_PATH, '', $file);
 		if (file_exists(WITY_PATH.$file)) {
 			// WTemplate automatically adds the base directory defined in WSystem::getTemplate()
@@ -135,19 +136,19 @@ class WView {
 			WNote::error('view_set_template', "WView::setTemplate(): The template file \"".$file."\" does not exist.");
 		}
 	}
-	
+
 	/**
 	 * Returns the template file configured for the current view
-	 * 
+	 *
 	 * @return string Template file href
 	 */
 	public function getTemplate() {
 		return $this->templateFile;
 	}
-	
+
 	/**
 	 * Assigns a list of variables whose names are in $names to their $values
-	 * 
+	 *
 	 * @param mixed $names  variable names
 	 * @param mixed $values variable values
 	 */
@@ -160,11 +161,11 @@ class WView {
 			}
 		}
 	}
-	
+
 	/**
 	 * Assigns a variable whose name is $name to a $value
-	 * 
-	 * @param mixed $name   variable name 
+	 *
+	 * @param mixed $name   variable name
 	 * @param mixed $value  variable value
 	 */
 	public function assignOne($name, $value) {
@@ -177,10 +178,10 @@ class WView {
 			$this->vars[$name] = $value;
 		}
 	}
-	
+
 	/**
 	 * Get the value of one assigned variable
-	 * 
+	 *
 	 * @param string $name
 	 * @return mixed
 	 */
@@ -190,27 +191,27 @@ class WView {
 		}
 		return null;
 	}
-	
+
 	/**
 	 * Some variables may be considered as global vars in a way that they will have a
 	 * particular treatment when they will be assigned in the template compilator.
 	 * This treatment is defined in this function.
 	 * Global vars are not erased. If two different values are assigned to a same global var,
 	 * they will stack in an array.
-	 * 
+	 *
 	 * For instance, $css and $js are considered as global vars since they will be automaticly
 	 * inserted in a <script> or <link> html tag.
 	 * $this->assign('css', 'style.css');
 	 * {$css} will be replaced by <link href="THEMES_DIR/style.css" rel="stylesheet" type="text/css" />
-	 * 
+	 *
 	 * @param string $stack_name stack name
 	 * @return string variable value
 	 */
 	public function getGlobalVar($stack_name) {
-		if (empty(self::$global_vars[$stack_name])) {
+		if (empty(self::$global_vars[$stack_name]) && $stack_name != 'require') {
 			return '';
 		}
-		
+
 		switch ($stack_name) {
 			case 'css':
 				$css = '';
@@ -218,52 +219,75 @@ class WView {
 					$css .= '<link href="'.$file.'" rel="stylesheet" type="text/css" />'."\n";
 				}
 				return $css;
-			
+
 			case 'js':
 				$script = '';
 				foreach (self::$global_vars['js'] as $file) {
 					$script .= '<script type="text/javascript" src="'.$file.'"></script>'."\n";
 				}
 				return $script;
-			
+
+			case 'require':
+				if (!file_exists('libraries/libraries.json')) {
+					return '';
+				}
+
+				$require =
+					'<script type="text/javascript" src="{$base_url}/libraries/requirejs/require.js"></script>'."\n"
+					.'<script>'."\n"
+					.'require.config('.file_get_contents('libraries/libraries.json').');'."\n"
+				;
+
+				// If array not empty
+				if (self::$global_vars['require']) {
+					$require .= 'require(["'
+						.implode('", "', self::$global_vars['require']);
+					$require = substr($require, 0, -3);
+					$require .= ']);'."\n";
+				}
+
+				$require .= '</script>'."\n";
+
+				return $require;
+
 			default:
 				return self::$global_vars[$stack_name];
 		}
 	}
-	
+
 	/**
 	 * Define the theme for this view
-	 * 
+	 *
 	 * @param string $theme
 	 */
 	public function setTheme($theme) {
 		$this->theme = $theme;
 	}
-	
+
 	/**
 	 * Get the theme name for this view
-	 * 
+	 *
 	 * @return string Theme name
 	 */
 	public function getTheme() {
 		return $this->theme;
 	}
-	
+
 	/**
 	 * Renders the view
-	 * 
+	 *
 	 * @return string The rendered string of the view
 	 */
 	public function render() {
 		$signature = $this->getContext('signature');
-		
+
 		// Check template file
 		if (empty($this->templateFile)) {
 			// WNote::error('view_template', "WView::render(): No template file found in the view ".$this->getName().".");
 			// A View can now be empty
 			return '';
 		}
-		
+
 		// Prevent Views from self inclusion more than 5 times
 		if (!isset($this->render_counts[$signature])) {
 			$this->render_counts[$signature] = 1;
@@ -271,32 +295,32 @@ class WView {
 			if ($this->render_counts[$signature] >= 5) {
 				return WNote::getView(array(WNote::error('WView::render', 'The view of this application may contain a problem: it tried to include itself more than 5 times. ')))->render();
 			}
-			
+
 			$this->render_counts[$signature]++;
 		}
-		
+
 		// Treat global vars
 		foreach (self::$global_vars as $stack => $values) {
 			$data = $this->getGlobalVar($stack);
-			
+
 			if (!empty($data)) {
 				$this->tpl->assign($stack, $data, true);
 			}
 		}
-		
+
 		// Switch context in WTemplate
 		$this->tpl->pushContext();
-		
+
 		// Assign View variables
 		$this->tpl->assign($this->vars);
-		
+
 		// Render the view
 		$file = $this->getTemplate();
 		$this->rendered_string = $this->tpl->parse($file);
-		
+
 		// Add the signature to the forms within the view
 		$this->rendered_string = preg_replace_callback(
-			'#<form([^>]*)>(<input type="hidden" name="form_signature")?#', 
+			'#<form([^>]*)>(<input type="hidden" name="form_signature")?#',
 			function($matches) use($signature) {
 				// Add action in the form values
 				preg_match('#action="([^"]*)"#', $matches[1], $matches_args);
@@ -304,7 +328,7 @@ class WView {
 				if (!empty($matches_args[1])) {
 					$action = '<input type="hidden" name="form_action" value="'.trim($matches_args[1], ' /').'" />';
 				}
-				
+
 				// Prevent from overriding sub-forms signatures
 				if (empty($matches[2])) {
 					return '<form'.$matches[1].'><input type="hidden" name="form_signature" value="'.$signature.'" />'.$action;
@@ -314,17 +338,17 @@ class WView {
 			},
 			$this->rendered_string
 		);
-		
+
 		// Come back to previous context
 		$this->tpl->popContext();
-		
+
 		// Clean the view for the next render
 		$this->templateFile = '';
 		$this->context['signature'] = '';
-		
+
 		return $this->rendered_string;
 	}
-	
+
 	/**
 	 * Retrieves the last rendered view string
 	 */
