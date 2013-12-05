c7f080fd26860c90639f3309b8e975e921d57a52
Johan <fofif_ii@hotmail.fr>
2013-12-05 16:54:19 +0100

    WRoute: clears the custom definition

--- system/WCore/WRoute.php
+++ system/WCore/WRoute.php
@@ -227,7 +227,7 @@ class WRoute {
 	 * @return boolean true if the redirection was applied
 	 */
 	public static function defineCustom($uri, $redirection) {
-		$uri = trim($uri, '/');
+		$uri = '/'.trim($uri, '/');
 		if (!empty($uri)) {
 			WConfig::set('route.custom.'.$uri, $redirection);
 			WConfig::save('route');
@@ -244,7 +244,7 @@ class WRoute {
 	 * @param string $uri The custom route to remove
 	 */
 	public static function deleteCustom($uri) {
-		$uri = trim($uri, '/');
+		$uri = '/'.trim($uri, '/');
 		if (!empty($uri) && !is_null(WConfig::get('route.custom.'.$uri))) {
 			$custom_routes = WConfig::get('route.custom');
 			unset($custom_routes[$uri]);
