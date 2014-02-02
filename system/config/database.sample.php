944e9411bbb8c4c24eca437469ee5e90f59bdf87
Johan <johandufau@gmail.com>
2014-02-02 15:29:49 +0100

    Cleans sample configuration files

--- system/config/database.sample.php
+++ system/config/database.sample.php
@@ -1,14 +1,15 @@
 <?php
-
 /**
- * Database configuration file
+ * Sample configuration file for system/config/database.php
+ * 
+ * This file defines the credentials to login to the system database.
  */
 
 $database = array(
     'server' => '127.0.0.1',
-    'port' => '',
-    'user' => 'root',
-    'pw' => '',
+    'port'   => '',
+    'user'   => 'root',
+    'pw'     => '',
     'dbname' => 'test',
     'prefix' => ''
 );
