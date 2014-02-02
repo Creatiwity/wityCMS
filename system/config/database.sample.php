605d42f0e725682c57496e7559c0c499c9ec6587
Johan <johandufau@gmail.com>
2014-02-02 21:09:07 +0100

    Improves database.sample.php

--- system/config/database.sample.php
+++ system/config/database.sample.php
@@ -2,16 +2,16 @@
 /**
  * Sample configuration file for system/config/database.php
  * 
- * This file defines the credentials to login to the system database.
+ * This file defines the credentials to login to the MySQL database.
  */
 
 $database = array(
     'server' => '127.0.0.1',
-    'port'   => '',
-    'user'   => 'root',
-    'pw'     => '',
-    'dbname' => 'test',
-    'prefix' => ''
+    'port'   => '',          // port for MySQL (usually empty)
+    'user'   => 'root',      // user name
+    'pw'     => '',          // password
+    'dbname' => 'witycms',   // database's name
+    'prefix' => ''           // prefix used to name tables in this database
 );
 
 ?>
