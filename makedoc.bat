60c89049ff09bb3af1bd3daa3a7615389b2c2245
Johan <johandufau@gmail.com>
2014-03-11 09:59:33 +0100

    Deletes empty file + update doc generator

--- makedoc.bat
+++ makedoc.bat
@@ -1,3 +1,3 @@
 @echo off
 set PWD=%~dp0
-%PHP_PEAR_BIN_DIR%\phpdoc -d %PWD% -t %PWD%doc --title WityCMS --ignore cache/,libraries/,helpers/fpdf/,helpers/phpmailer/,helpers/upload/,helpers/SimpleImage/ --template responsive
+%PHP_PEAR_BIN_DIR%\phpdoc -d %PWD% -t %PWD%doc --title WityCMS --ignore cache/,libraries/,helpers/fpdf/,helpers/fMailbox/,helpers/phpmailer/,helpers/upload/,helpers/SimpleImage/ --template responsive
