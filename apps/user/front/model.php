0ea27e5424852d3a1889ceb3cfe21c31641fda06
Johan <johandufau@gmail.com>
2014-12-15 22:55:28 +0100

    Adds isEmail into WTools

--- apps/user/front/model.php
+++ apps/user/front/model.php
@@ -57,7 +57,7 @@ class UserModel {
 	 * @return mixed true if valid or error string
 	 */
 	public function checkEmail($email) {
-		if (empty($email) || !preg_match('#^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$#i', $email)) {
+		if (!WTools::isEmail($email)) {
 			return 'email_not_valid';
 		}
 		
--- system/WCore/WTools.php
+++ system/WCore/WTools.php
@@ -13,12 +13,28 @@ defined('IN_WITY') or die('Access denied');
  * @version 0.5.0-dev-09-01-2013
  */
 class WTools {
+	/**
+	 * Removes accents from a string.
+	 * 
+	 * @param string $string
+	 * @return string
+	 */
 	public static function stripAccents($string) {
 		return strtr(
 			utf8_decode($string), 
 			utf8_decode('àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ'), 
 			'aaaaaceeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUY'
 		);
+	}
+	
+	/**
+	 * Verifies whether a string is a valid email.
+	 * 
+	 * @param string $string
+	 * @return bool
+	 */
+	public static function isEmail($string) {
+		return (!empty($string) && preg_match('#^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$#i', $string));
 	}
 }
 
