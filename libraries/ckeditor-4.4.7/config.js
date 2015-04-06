fcc66914d1d0ac9ec8d7b32490f433b149c0b010
Johan <johandufau@gmail.com>
2015-04-06 14:19:48 +0200

    Updates CKEditor skin

--- libraries/ckeditor-4.4.7/config.js
+++ libraries/ckeditor-4.4.7/config.js
@@ -7,4 +7,10 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+
+	config.extraPlugins = 'youtube';
+	
+	config.stylesSet = [
+		//{ name: 'Arial', element: 'span', styles: { 'font-family': 'Arial' } }
+	];
 };
