5130065408208979397bbbea6e871ae35be21e54
Matthieu <matthieuraymond@hotmail.com>
2015-07-31 19:21:10 +0200

    Add insert and update automation functions in core

--- system/WCore/WDatabase.php
+++ system/WCore/WDatabase.php
@@ -10,6 +10,7 @@ defined('WITYCMS_VERSION') or die('Access denied');
  * 
  * @package System\WCore
  * @author Johan Dufau <johan.dufau@creatiwity.net>
+ * @author Matthieu Raymond <matthieu.raymond@creatiwity.net>
  * @version 0.5.0-dev-29-11-2013
  */
 class WDatabase extends PDO {
@@ -141,6 +142,85 @@ class WDatabase extends PDO {
 		$querystring = $this->prefixTables($querystring);
 		
 		return parent::prepare($querystring, $driver_options);
+	}
+	
+	/** 
+	 * Insert a new row in table and return its id
+	 *
+	 * @param string        $table table name
+	 * @param array(string) $fields fields to insert
+	 * @param array(*)      $data data to insert (same key as in $fields)
+	 *
+	 * @return int or false id of inserted row or failure
+	 */
+	public function insertInto($table, $fields, $data) {
+		$req = 'INSERT INTO '.$table.'(';
+
+		foreach ($fields as $key) {
+			$req .= $key.', ';
+		}
+
+		if (count($fields) > 1) {
+			$req = substr($req, 0, -2);
+		}
+
+		$req .= ') VALUES (';
+
+		foreach ($fields as $key) {
+			$req .= ':'.$key.', ';
+		}
+
+		if (count($fields) > 1) {
+			$req = substr($req, 0, -2);
+		}
+
+		$req .= ')';
+
+		$prep = $this->prepare($req);
+
+		foreach ($fields as $key) {
+			$data[$key] = isset($data[$key]) ? $data[$key] : '';
+			$prep->bindParam(':'.$key, $data[$key]);
+		}
+		
+		if ($prep->execute()) {
+			return $this->lastInsertId();
+		} else {
+			return false;
+		}
+	}
+
+	/** 
+	 * Update a table and return the numbers of row affected
+	 *
+	 * @param string        $table table name
+	 * @param array(string) $fields fields to update
+	 * @param array(*)      $data data to update (same key as in $fields)
+	 * @param string        $cond condition(s) of update (all rows by default)
+	 *
+	 * @return int or false number of inserted row or failure
+	 */
+	public function update($table, $fields, $data, $cond = '1') {
+		$req = 'UPDATE '.$table.' SET ';
+
+		foreach ($fields as $key) {
+			$req .= $key.' = :'.$key.', ';
+		}
+
+		if (count($fields) > 1) {
+			$req = substr($req, 0, -2);
+		}
+
+		$req .= ' WHERE '.$cond;
+
+		$prep = $this->prepare($req);
+
+		foreach ($fields as $key) {
+			$data[$key] = isset($data[$key]) ? $data[$key] : '';
+			$prep->bindParam(':'.$key, $data[$key]);
+		}
+		
+		return $prep->execute();
 	}
 }
 
