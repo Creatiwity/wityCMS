5080437cf0939229467688b50756b20739402b69
Johan Dufau <johan.dufau@creatiwity.net>
2015-08-23 22:05:45 +0200

    Improves page app

--- apps/page/front/view.php
+++ apps/page/front/view.php
@@ -7,21 +7,20 @@ defined('WITYCMS_VERSION') or die('Access denied');
 
 /**
  * PageView is the Front View of the Page Application
- * 
+ *
  * @package Apps\Page\Front
  * @author Johan Dufau <johan.dufau@creatiwity.net>
  * @author Julien Blatecky <julien.blatecky@creatiwity.net>
- * @version 0.5.0-dev-23-03-2015
+ * @version 0.5.0-dev-23-08-2015
  */
 class PageView extends WView {
 	public function display(array $model) {
-		$this->tpl->assign('side_pages', $model['side_pages']);
-		
-		// Assign meta data
+		$this->assign('css', '/apps/page/front/css/page.css');
+
 		$context = $this->getContext();
 
 		if (!$context['parent']) {
-			$this->tpl->assign('wity_page_title', $model['page']['meta_title']);
+			$this->tpl->assign('wity_page_title', $model['page']['meta_title'].' - '.WConfig::get('config.site_title'));
 			$this->tpl->assign('wity_page_description', $model['page']['meta_description']);
 		}
 
