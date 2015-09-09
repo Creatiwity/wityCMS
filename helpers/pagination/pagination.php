07d61b341e864d355c235e3ce164dc50f042f63e
Matthieu <matthieuraymond@hotmail.com>
2015-09-09 11:40:17 +0200

    Fixes on url with % inside (get request for examples)

--- helpers/pagination/pagination.php
+++ helpers/pagination/pagination.php
@@ -11,41 +11,41 @@
  * @version 0.3-01-04-2010
  */
 class Pagination {
-    
-    /**
-     *
-     * @var int Total pages number 
-     */
+	
+	/**
+	 *
+	 * @var int Total pages number 
+	 */
 	private $n;
-    /**
-     *
-     * @var int Total elements number 
-     */
+	/**
+	 *
+	 * @var int Total elements number 
+	 */
 	private $total;
-    /**
-     *
-     * @var int Maximum number of elements on a page
-     */
+	/**
+	 *
+	 * @var int Maximum number of elements on a page
+	 */
 	private $limit;
-    /**
-     *
-     * @var int Current page 
-     */
+	/**
+	 *
+	 * @var int Current page 
+	 */
 	private $currentPage;
-    /**
-     *
-     * @var string URL pattern for the current page
-     */
+	/**
+	 *
+	 * @var string URL pattern for the current page
+	 */
 	private $urlPattern;
 	
-    /**
-     * Setup Pagination
-     * 
-     * @param type  $total          Total elements number
-     * @param type  $limit          Maximum number of elements on a page
-     * @param int   $currentPage    Page Current page
-     * @param type  $urlPattern     URL model for the current page
-     */
+	/**
+	 * Setup Pagination
+	 * 
+	 * @param type  $total          Total elements number
+	 * @param type  $limit          Maximum number of elements on a page
+	 * @param int   $currentPage    Page Current page
+	 * @param type  $urlPattern     URL model for the current page
+	 */
 	public function __construct($total, $limit, $currentPage, $urlPattern) {
 		$this->total = $total <= 0 ? 1 : $total;
 		$this->limit = $limit;
@@ -60,11 +60,11 @@ class Pagination {
 		$this->currentPage = $currentPage;
 	}
 	
-    /**
-     * Returns the HTML code corresponding to the current page page-selector
-     * 
-     * @return string HTML code of the page selector
-     */
+	/**
+	 * Returns the HTML code corresponding to the current page page-selector
+	 * 
+	 * @return string HTML code of the page selector
+	 */
 	public function getHTML() {
 		// CSS adding
 		$tpl = WSystem::getTemplate();
@@ -81,9 +81,9 @@ class Pagination {
 			}
 			// We are around the current page : we display it
 			else if (abs($this->currentPage - $i) <= 5) {
-				$output .= sprintf('<li><a href="'.$this->urlPattern.'">%d</a></li> ', $i, $i);
+				$output .= sprintf('<li><a href="'.urldecode($this->urlPattern).'">%d</a></li> ', $i, $i);
 			}
-            // Displaying something before forgetting useless pages
+			// Displaying something before forgetting useless pages
 			else {
 				// Before the current page
 				if (!$firstDone && $i < $this->currentPage) {
@@ -91,7 +91,7 @@ class Pagination {
 					$output .= sprintf(
 						'<li><a href="%s">First</a></li>
 						<li><a href="%s" title="Page précédente">&laquo;</a></li> ', 
-						sprintf($this->urlPattern, 1), sprintf($this->urlPattern, $this->currentPage-1)
+						sprintf(urldecode($this->urlPattern), 1), sprintf(urldecode($this->urlPattern), $this->currentPage-1)
 					);
 				}
 				// After the current page
@@ -100,7 +100,7 @@ class Pagination {
 					$output .= sprintf(
 						'<li><a href="%s" title="Page suivante">&raquo;</a></li>
 						<li><a href="%s">Last</a></li>', 
-						sprintf($this->urlPattern, $this->currentPage+1), sprintf($this->urlPattern, $this->n)
+						sprintf(urldecode($this->urlPattern), $this->currentPage+1), sprintf(urldecode($this->urlPattern), $this->n)
 					);
 				}
 				// After interesting cases : stop
@@ -114,12 +114,12 @@ class Pagination {
 		return $output;
 	}
 	
-    /**
-     * Returns the HTML code corresponding to the current page page-selector
-     * 
-     * @see Pagination::getHTML()
-     * @return string HTML code of the page selector
-     */
+	/**
+	 * Returns the HTML code corresponding to the current page page-selector
+	 * 
+	 * @see Pagination::getHTML()
+	 * @return string HTML code of the page selector
+	 */
 	public function __toString() {
 		return $this->getHTML();
 	}
