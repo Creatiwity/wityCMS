67a8f1b191d0887418ab9ea67b5065cf3cad27ad
Johan Dufau <johan.dufau@creatiwity.net>
2015-10-09 16:32:43 +0200

    WNote: fixes log handler

--- system/WCore/WNote.php
+++ system/WCore/WNote.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * WNote.php
  */
@@ -7,7 +7,7 @@ defined('WITYCMS_VERSION') or die('Access denied');
 
 /**
  * WNote manages all notes : stores, displays, ...
- * 
+ *
  * @package System\WCore
  * @author Johan Dufau <johan.dufau@creatiwity.net>
  * @version 0.5.0-dev-09-06-2013
@@ -19,15 +19,15 @@ class WNote {
 	const ERROR   = 'danger';
 	const INFO    = 'info';
 	const SUCCESS = 'success';
-	
+
 	/**
 	 * @var array Notes to be displayed in a plain view
 	 */
 	private static $plain_stack = array();
-	
+
 	/**
 	 * Raises an ERROR note
-	 * 
+	 *
 	 * @param  string $code      note's code
 	 * @param  string $message   note's message
 	 * @param  string $handlers  handlers (several handlers separated by a comma can be specified)
@@ -40,15 +40,15 @@ class WNote {
 
 		return self::raise(array(
 			'level'    => self::ERROR,
-			'code'     => $code, 
-			'message'  => $message, 
+			'code'     => $code,
+			'message'  => $message,
 			'handlers' => $handlers
 		));
 	}
-	
+
 	/**
 	 * Raises an INFO note
-	 * 
+	 *
 	 * @param  string $code      note's code
 	 * @param  string $message   note's message
 	 * @param  string $handlers  handlers (several handlers separated by a comma can be specified)
@@ -61,15 +61,15 @@ class WNote {
 
 		return self::raise(array(
 			'level'    => self::INFO,
-			'code'     => $code, 
-			'message'  => $message, 
+			'code'     => $code,
+			'message'  => $message,
 			'handlers' => $handlers
 		));
 	}
-	
+
 	/**
 	 * Raises a SUCCESS note
-	 * 
+	 *
 	 * @param  string $code      note's code
 	 * @param  string $message   note's message
 	 * @param  string $handlers  handlers (several handlers separated by a comma can be specified)
@@ -82,15 +82,15 @@ class WNote {
 
 		return self::raise(array(
 			'level'    => self::SUCCESS,
-			'code'     => $code, 
-			'message'  => $message, 
+			'code'     => $code,
+			'message'  => $message,
 			'handlers' => $handlers
 		));
 	}
-	
+
 	/**
 	 * Raises a new note
-	 * 
+	 *
 	 * @param  array $note The note as an array(level, code, message, handlers)
 	 * @return array The same note given in argument
 	 */
@@ -112,23 +112,23 @@ class WNote {
 				self::handle_plain($note);
 			}
 		}
-		
+
 		return $note;
 	}
-	
+
 	/**
 	 * Ignores the note
-	 * 
+	 *
 	 * @param array $note Note as returned by WNote::raise()
 	 * @return void
 	 */
 	public static function handle_ignore(array $note) {
 		// do nothing...
 	}
-	
+
 	/**
 	 * Returns an HTML form of the note
-	 * 
+	 *
 	 * @param array $note Note as returned by WNote::raise()
 	 * @return string HTML form of the note
 	 */
@@ -138,22 +138,22 @@ class WNote {
 			."<li><strong>message:</strong> ".$note['message']."</li>\n"
 			."</ul><br />\n";
 	}
-	
+
 	/**
 	 * Displays the note in an HTML format and then, kills the script
-	 * 
+	 *
 	 * @param array $note Note as returned by WNote::raise()
 	 */
 	public static function handle_die(array $note) {
 		static $died = false; // prevent from looping
 		if (!$died) {
 			$died = true;
-			
+
 			try {
 				// Try to display the note an artistic way
 				self::handle_plain($note);
 				$plain_view = self::getPlainView();
-				
+
 				if (!is_null($plain_view)) {
 					$response = new WResponse();
 					if ($response->render($plain_view, '_blank')) {
@@ -161,16 +161,16 @@ class WNote {
 					}
 				}
 			} catch (Exception $e) {}
-			
+
 			// Default HTML display
 			echo self::handle_html($note);
 			die;
 		}
 	}
-	
+
 	/**
 	 * Adds a note in the SESSION variable stack in order to display it when rendering the whole page
-	 * 
+	 *
 	 * @param array $note Note as returned by WNote::raise()
 	 * @see WNote::count()
 	 * @see WNote::get()
@@ -183,21 +183,21 @@ class WNote {
 			$_SESSION['notes'][] = $note;
 		}
 	}
-	
+
 	/**
 	 * Handles note to be displayed in a plain HTML View.
 	 * Often used for fail-over purposes (a view did not manage to render since theme cannot be found for instance).
-	 * 
+	 *
 	 * @param array $note Note as returned by WNote::raise()
 	 */
 	public static function handle_plain(array $note) {
 		self::$plain_stack[] = $note;
 	}
-	
+
 	/**
 	 * Log handler
 	 * Stores the note in a log file (system/wity.log)
-	 * 
+	 *
 	 * @param array $note Note as returned by WNote::raise()
 	 */
 	public static function handle_log(array $note) {
@@ -206,27 +206,27 @@ class WNote {
 			$log_files_count = count(glob(LOGS_DIR.'*.log'));
 			rename(LOGS_DIR.'wity.log', LOGS_DIR.'wity.'.$log_files_count.'.log');
 		}
-		
+
 		$file = fopen(LOGS_DIR.'wity.log', 'a+');
 		$date = new WDate();
-		$text = sprintf("[%s] [%s] [user %s|%s] [route %s] %s - %s\r\n",
+		$text = sprintf("[%s] [level %s] [user %s|%s] [route %s] [user-agent %s] %s - %s\r\n",
 			$date->__toString(),
 			$note['level'],
 			@$_SESSION['userid'],
 			@WSession::getIP(),
-			$_SERVER['HTTP_USER_AGENT'],
 			$_SERVER['REQUEST_URI'],
+			$_SERVER['HTTP_USER_AGENT'],
 			$note['code'],
 			$note['message']
 		);
 		fwrite($file, $text);
 		fclose($file);
 	}
-	
+
 	/**
 	 * Email handler
 	 * Sends the note by email to the administrator (defined in config/config.php)
-	 * 
+	 *
 	 * @param array $note Note as returned by WNote::raise()
 	 */
 	public static function handle_email(array $note) {
@@ -256,11 +256,11 @@ class WNote {
 			unset($mail);
 		}
 	}
-	
+
 	/**
 	 * Debug handler
 	 * If the debug mode is activated, email and log handlers will be trigered.
-	 * 
+	 *
 	 * @param array $note Note as returned by WNote::raise()
 	 */
 	public static function handle_debug(array $note) {
@@ -269,16 +269,16 @@ class WNote {
 			self::handle_email($note);
 		}
 	}
-	
+
 	/**
 	 * Returns and unset from the SESSION stack all notes whose $code is matching the $pattern
-	 * 
+	 *
 	 * @param string $pattern optional pattern to find a note by its code
 	 * @return array All notes having its $code matching the $pattern
 	 */
 	public static function get($pattern = '*') {
 		$result = array();
-		
+
 		if (!empty($_SESSION['notes'])) {
 			foreach ($_SESSION['notes'] as $code => $note) {
 				if ($pattern == '*' || $code == $pattern || (strpos($pattern, '*') !== false && preg_match('#'.str_replace('*', '.*', $pattern).'#', $code))) {
@@ -288,38 +288,38 @@ class WNote {
 				}
 			}
 		}
-		
+
 		return $result;
 	}
-	
+
 	/**
 	 * Parses a set of notes and returns the html response
-	 * 
+	 *
 	 * @param array $notes Set of notes that will be parsed
 	 * @return string The HTML response
 	 */
 	public static function getView(array $notes_data) {
 		static $css_added = false;
-		
+
 		if (empty($notes_data)) {
 			return new WView();
 		}
-		
+
 		// Remove the notes from the stack
 		foreach ($notes_data as $note) {
 			unset($_SESSION['notes'][$note['code']]);
 		}
-		
+
 		$view = new WView();
 		$view->assign('notes_data', $notes_data);
 		$view->setTemplate('themes/system/note/note_view.html');
-		
+
 		return $view;
 	}
-	
+
 	/**
 	 * Gets the plain notes.
-	 * 
+	 *
 	 * @return array
 	 */
 	public static function getPlain() {
@@ -327,10 +327,10 @@ class WNote {
 		self::$plain_stack = array();
 		return $plain_notes;
 	}
-	
+
 	/**
 	 * Prepares a view to display a set of notes in a fall-back view
-	 * 
+	 *
 	 * @return WView
 	 */
 	public static function getPlainView() {
@@ -341,7 +341,7 @@ class WNote {
 			$view->assign('css', '/themes/system/note/note_plain.css');
 			$view->assign('notes_data', self::getPlain());
 			$view->setTemplate('themes/system/note/note_plain_view.html');
-			
+
 			return $view;
 		}
 		return null;
