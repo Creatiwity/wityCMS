d5c1754cd9be63a95251bec2876e8c935c90550b
Johan Dufau <johan.dufau@creatiwity.net>
2016-02-10 16:58:09 +0100

    [Search] Adds application - closes #103

---
+++ apps/search/manifest.php
@@ -0,0 +1,14 @@
+<?php defined('WITYCMS_VERSION') or die('Access denied'); ?>
+<?xml version="1.0" encoding="utf-8" ?>
+<app>
+	<!-- Application name -->
+	<name>Search</name>
+
+	<version>0.5.0-dev-02-01-2015</version>
+
+	<!-- Last update date -->
+	<date>23-03-2015</date>
+
+	<!-- Front pages -->
+	<action default="default">form</action>
+</app>
