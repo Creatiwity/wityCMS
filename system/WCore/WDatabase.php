894088647d82d83cffa0f9cabe416fe55a475bb0
Johan Dufau <johan.dufau@creatiwity.net>
2016-11-16 15:36:35 +0100

    [DB] Adds an escape machanism to prevent from prefixing

--- system/WCore/WDatabase.php
+++ system/WCore/WDatabase.php
@@ -70,9 +70,11 @@ class WDatabase extends PDO {
 	private function prefixTables($querystring) {
 		if (!empty($this->tablePrefix)) {
 			foreach ($this->tables as $table) {
-				$querystring = preg_replace('#([^a-z0-9_])'.$table.'([^a-z0-9_]|$)#', '$1'.$this->tablePrefix.$table.'$2', $querystring);
+				$querystring = preg_replace('#([^a-z\\\\0-9_])'.$table.'([^a-z0-9_]|$)#', '$1'.$this->tablePrefix.$table.'$2', $querystring);
 			}
 		}
+
+		$querystring = str_replace('\\', '', $querystring);
 
 		return $querystring;
 	}
@@ -180,7 +182,7 @@ class WDatabase extends PDO {
 
 		foreach ($fields as $key) {
 			$data[$key] = isset($data[$key]) ? $data[$key] : '';
-			$prep->bindParam(':'.$key, $data[$key]);
+			$prep->bindParam(':'.str_replace('\\', '', $key), $data[$key]);
 		}
 
 		if ($prep->execute()) {
@@ -217,7 +219,7 @@ class WDatabase extends PDO {
 
 		foreach ($fields as $key) {
 			$data[$key] = isset($data[$key]) ? $data[$key] : '';
-			$prep->bindParam(':'.$key, $data[$key]);
+			$prep->bindParam(':'.str_replace('\\', '', $key), $data[$key]);
 		}
 
 		return $prep->execute();
